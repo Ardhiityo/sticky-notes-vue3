4dcc76fc1eb90cd556fedd01fbf7f7920fd0c51d
belajar-vue-2 <aryaadi229@gmail.com>
2024-06-02 21:03:33 +0700

    Membuat Ui Bagian Form Modal

--- src/App.vue
+++ src/App.vue
@@ -13,14 +13,23 @@ import { reactive } from 'vue';
     </header>
 
     <section>
-      
-      <div>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta culpa pariatur expedita ipsum quae autem, obcaecati, beatae fuga eos officiis, non ut possimus cumque quaerat consectetur saepe ea sunt sed.</p>
+
+      <div class="content">
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta culpa pariatur expedita ipsum quae autem,
+          obcaecati, beatae fuga eos officiis, non ut possimus cumque quaerat consectetur saepe ea sunt sed.</p>
         <p>12/12/2023</p>
       </div>
-      
+
     </section>
   </main>
+
+  <div class="modal">
+    <div class="content-modal">
+      <button class="btn-close">X</button>
+      <textarea class="modal-text" name="modal" cols="30" rows="10"></textarea>
+      <button class="btn-save">Save</button>
+    </div>
+  </div>
 
 </template>
 
@@ -58,7 +67,7 @@ section {
   flex-wrap: wrap;
 }
 
-div {
+.content {
   width: 30%;
   padding: 15px;
   background-color: rgb(124, 202, 204);
@@ -67,6 +76,64 @@ div {
   flex-direction: column;
   justify-content: space-between;
   border-radius: 5px;
+}
+
+.modal {
+  background-color: rgba(234, 175, 175, 0.29);
+  position: absolute;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.content-modal {
+  background-color: rgb(212, 212, 212);
+  position: relative;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  width: 40%;
+  padding: 30px;
+  height: 50%;
+  box-sizing: border-box;
+  border-radius: 10px;
+}
+
+.btn-close {
+  position: absolute;
+  top: 5px;
+  right: 5px;
+  border: none;
+  padding: 5px 8px;
+  border-radius: 4px;
+  cursor: pointer;
+  background-color: transparent;
+  font-size: large;
+}
+
+.modal-text {
+  width: 100%;
+  height: 100%;
+  border-radius: 5px;
+}
+
+.btn-save {
+  margin-top: 10px;
+  display: flex;
+  width: 100%;
+  padding: 10px;
+  border-radius: 4px;
+  display: flex;
+  justify-content: center;
+  border: none;
+  cursor: pointer;
+  background-color: rgb(192, 192, 192);
+  font-size: large;
 }
 
 </style>
